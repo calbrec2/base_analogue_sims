1e1d331ff0340e443099584a837b4cf41328ada3
calbrec2 <calbrec2@uoregon.edu>
2023-12-15 08:06:31 -0800

    added data for one dat of FPGA and one day of lock-in so we can test the data file grabber

---
+++ computerMode.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec 15 07:04:05 2023
+
+@author: calbrec2
+"""
+
+# write a code to find the computer terminal of the machine being used so we can 
+# load data files easily without having to edit the paths
+
+
+import os
+import glob
+
+# def computerMode():
+    
+path = os.getcwd()
